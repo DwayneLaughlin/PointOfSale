6aa09729bf7dd766cb3a1978d5be902ea4b8ad67
Dwayne Laughlin <dr2@ameritech.net>
2024-08-11 14:25:32 -0500

    Add files via upload

---
+++ OrderTests.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections.Generic;
+using FastFoodPOS.FoodItems.Class;
+using FastFoodPOS.FoodItems.Condiments;
+using FastFoodPOS.FoodItems.Interfaces;
+using Xunit;
+namespace FastFoodPOS.Tests;
+
+public class OrderTests
+{
+    [Fact]
+    public void TestAddCondimentKetchupOk()
+    {
+        //Arrange
+        Order order = new Order();
+        string ketchup = "ketchup";
+
+        //Act
+        order.AddCondiment(ketchup);
+
+        //Assert
+        List<IFood> orderList = order.GetOrder();
+        Assert.NotEmpty(orderList);
+        Assert.Single(orderList);
+        Assert.Equal(.25, orderList.First().GetPrice());
+        Assert.IsType<Ketchup>(orderList.First());
+    }
+
+    [Fact]
+    public void TestAddCondimentMustardOk()
+    {
+        //Arrange
+        Order order = new Order();
+        string mustard = "mustard";
+
+        //Act
+        order.AddCondiment(mustard);
+
+        //Assert
+        List<IFood> orderList = order.GetOrder();
+        Assert.NotEmpty(orderList);
+        Assert.Single(orderList);
+        Assert.Equal(.25, orderList.First().GetPrice());
+        Assert.IsType<Mustard>(orderList.First());
+    }
+
+    [Fact]
+    public void TestAddCondimentKetchupQuantityException()
+    {
+        //Arrange
+        Order order = new Order();
+        List<IFood> list = new List<IFood>()
+        {
+            new Ketchup(),
+            new Ketchup(),
+            new Ketchup()
+        };
+        
+        order._foodItems = list;
+
+        //Act & Assert
+        Assert.Throws<Exception>(() => order.AddCondiment("ketchup"));
+    }
+
+    [Fact]
+    public void TestAddCondimentMustardQuantityException()
+    {
+        //Arrange
+        Order order = new Order();
+        List<IFood> list = new List<IFood>()
+        {
+            new Mustard(),
+            new Mustard(),
+            new Mustard()
+        };
+        
+        order._foodItems = list;
+
+
+        //Act & Assert
+        Assert.Throws<Exception>(() => order.AddCondiment("mustard"));
+    }
+
+    [Fact]
+    public void TestAddFoodBurgerOk()
+    {
+        //Arrange
+        Order order = new Order();
+
+        //Act
+        order.AddFood("burger", "small");
+
+        //Assert
+        List<IFood> result = order._foodItems;
+        Assert.NotNull(result);
+        Assert.Single(result);
+        Assert.IsType<Burger>(result.First());
+        Assert.Equal("small", result.First().GetSize());
+    }
+
+    [Fact]
+    public void TestAddFoodOnionRingsOk()
+    {
+        //Arrange
+        Order order = new Order();
+
+        //Act
+        order.AddFood("onionRings", "medium");
+
+        //Assert
+        List<IFood> result = order._foodItems;
+        Assert.NotNull(result);
+        Assert.Single(result);
+        Assert.IsType<Burger>(result.First());
+        Assert.Equal("medium", result.First().GetSize());
+    }
+
+    [Fact]
+    public void TestAddFoodChickenSandwichOk()
+    {
+        //Arrange
+        Order order = new Order();
+
+        //Act
+        order.AddFood("chickenSandwich", "small");
+
+        //Assert
+        List<IFood> result = order._foodItems;
+        Assert.NotNull(result);
+        Assert.Single(result);
+        Assert.IsType<ChickenSandwich>(result.First());
+        Assert.Equal("small", result.First().GetSize());
+    }
+
+    [Fact]
+    public void TestAddFoodFriesOk()
+    {
+        //Arrange
+        Order order = new Order();
+
+        //Act
+        order.AddFood("fries", "medium");
+
+        //Assert
+        List<IFood> result = order._foodItems;
+        Assert.NotNull(result);
+        Assert.Single(result);
+        Assert.IsType<Fries>(result.First());
+        Assert.Equal("medium", result.First().GetSize());
+    }
+
+    [Fact]
+    public void TestAddFoodMilkshakeOk()
+    {
+        //Arrange
+        Order order = new Order();
+
+        //Act
+        order.AddFood("milkshake", "large");
+
+        //Assert
+        List<IFood> result = order._foodItems;
+        Assert.NotNull(result);
+        Assert.Single(result);
+        Assert.IsType<Milkshake>(result.First());
+        Assert.Equal("large", result.First().GetSize());
+    }
+
+    [Fact]
+    public void TestAddFoodArgumentException()
+    {
+        //Arrange
+        Order order = new Order();
+
+        //Act & Assert
+        Assert.Throws<ArgumentException>(() => order.AddFood("turkey", "large"));
+    }
+}
---
+++ TransActionTests.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using FastFoodPOS.FoodItems.Class;
+using FastFoodPOS.FoodItems.Interfaces;
+using Xunit;
+namespace FastFoodPOS.Tests
+{
+	public class TransActionTests
+	{
+        [Fact]
+        public void TestCalculateOrderTotalOK()
+        {
+            //Arrange
+            Order order = new Order();
+            Burger burger1 = new Burger();
+            Burger burger2 = new Burger();
+            burger1.Size = "small";
+            burger2.Size = "medium";
+            double total = 17.50;
+            List<IFood> list = new List<IFood>()
+                {
+                    burger1,
+                    burger2
+                };
+            order._foodItems = list;
+            Transaction transaction = new Transaction(order);
+
+            //Act
+            double result = transaction.CalculateOrderTotal();
+
+            //Assert
+            Assert.Equal(total, result);
+
+        }
+
+        [Fact]
+        public void TestAddFundsOk()
+        {
+            //Arrange
+            Order order = new Order();
+            Transaction transaction = new Transaction(order);
+            double amount = 10;
+
+            //Act
+            double result = transaction.AddFunds(amount);
+
+            //Assert
+            Assert.Equal(amount, result);
+        }
+
+        [Fact]
+        public void TestAddFundsBigBillException()
+        {
+            //Arrange
+            Order order = new Order();
+            Transaction transaction = new Transaction(order);
+            double amount = 100;
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => transaction.AddFunds(amount));
+
+        }
+
+        [Fact]
+        public void TestAddFundsSmallCoinException()
+        {
+            //Arrange
+            Order order = new Order();
+            Transaction transaction = new Transaction(order);
+            double amount = .01;
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => transaction.AddFunds(amount));
+
+        }
+
+        [Fact]
+        public void TestAttemptPaymentNoChange()
+        {
+            //Arrange
+            Order order = new Order();
+            Burger burger1 = new Burger();
+            Burger burger2 = new Burger();
+            burger1.Size = "small";
+            burger2.Size = "medium";
+            List<IFood> list = new List<IFood>()
+                {
+                    burger1,
+                    burger2
+                };
+            order._foodItems = list;
+            Transaction transaction = new Transaction(order);
+
+            //Act
+            string result = transaction.AttemptPayment(17.50);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Contains("Payment Successful",result);
+            Assert.DoesNotContain("change", result);
+        }
+
+        [Fact]
+        public void TestAttemptPaymentChangeOwed()
+        {
+            //Arrange
+            Order order = new Order();
+            Burger burger1 = new Burger();
+            Burger burger2 = new Burger();
+            burger1.Size = "small";
+            burger2.Size = "medium";
+            List<IFood> list = new List<IFood>()
+                {
+                    burger1,
+                    burger2
+                };
+            order._foodItems = list;
+            Transaction transaction = new Transaction(order);
+
+            //Act
+            string result = transaction.AttemptPayment(18.00);
+
+            //Asser
+            Assert.NotNull(result);
+            Assert.Contains("change", result);
+        }
+
+        [Fact]
+        public void TestAttemptPaymentInsufficientFunds()
+        {
+            //Arrange
+            Order order = new Order();
+            Burger burger1 = new Burger();
+            Burger burger2 = new Burger();
+            burger1.Size = "small";
+            burger2.Size = "medium";
+            List<IFood> list = new List<IFood>()
+                {
+                    burger1,
+                    burger2
+                };
+            order._foodItems = list;
+            Transaction transaction = new Transaction(order);
+
+            //Act
+            string result = transaction.AttemptPayment(1.00);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Contains("Insufficient", result);
+        }
+    }
+}
+
